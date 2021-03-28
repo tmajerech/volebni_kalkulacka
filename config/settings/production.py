2d702b45e68c5222113a3b23ae5e96de5b603118
tmajerech <majerech.t@gmail.com>
2021-03-28 09:43:26 +0200

    test

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['tomasmajerech.cz', 'www.tomasmajerech.cz', '0.0.0.0', '167.99.254.121'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
